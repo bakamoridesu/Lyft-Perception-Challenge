0cae82f52ec56c72e6b6aa003db41ea2a5a3c0de
bakamoridesu <bakamoridesu@gmail.com>
2018-06-01 08:31:59 +0300

    New improved model 256x128. Fast and precise

--- Example/demo.py
+++ Example/demo.py
@@ -9,6 +9,10 @@ from glob import glob
 import pandas as pd
 import numpy as np
 import cv2
+#import time
+
+
+
 
 file = sys.argv[-1]
 
@@ -17,31 +21,35 @@ if file == 'demo.py':
     quit
 
 # Define encoder function
-def encode(array):
+def encode1(array):
     pil_img = Image.fromarray(array)
     buff = BytesIO()
     pil_img.save(buff, format="PNG")
     return base64.b64encode(buff.getvalue()).decode("utf-8")
 
+def encode(array):
+    enc = cv2.imencode('.PNG',array)[1]
+    b64 = base64.b64encode(enc)
+    return b64.decode("utf-8")
+
 video = skvideo.io.vread(file)
 
 answer_key = {}
 
-json_file = open('model_with_augmentation.json', 'r')
+json_file = open('model_with_augmentation_nearest.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 # load weights into new model
-loaded_model.load_weights("model_with_augmentation.h5")
-print("Loaded model from disc")
+loaded_model.load_weights("model_with_augmentation_nearest.h5")
 
 # Frame numbering starts at 1
 frame = 1
 
-for rgb_frame in video:
-    img = cv2.resize(img, (666,500))
+for img in video:
+    img = cv2.resize(img, (666,500), interpolation = cv2.INTER_NEAREST)
     img = np.expand_dims(img, axis=0)
-    pred = model.predict(img)
+    pred = loaded_model.predict(img)
     pred = pred[0]
     pred = pred.reshape(500,666,2)
     car_pred = pred[:,:,0]
@@ -50,16 +58,18 @@ for rgb_frame in video:
     car_pred_ = np.zeros(car_pred.shape)
     car_pred_[car_pred>0.3] = 1
     
+    
     road_pred_ = np.zeros(road_pred.shape)
     road_pred_[road_pred>0.3] = 1
     
     car_pred_ = cv2.resize(car_pred_, (800,600), interpolation = cv2.INTER_NEAREST)
     road_pred_ = cv2.resize(road_pred_, (800,600), interpolation = cv2.INTER_NEAREST)
-
+    road_pred_ = road_pred_.astype('uint8')
+    car_pred_ = car_pred_.astype('uint8')
     answer_key[frame] = [encode(car_pred_), encode(road_pred_)]
-    
     # Increment frame
     frame+=1
 
 # Print output in proper json format
+
 print (json.dumps(answer_key))
--- preinstall_script.sh
+++ preinstall_script.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # May need to uncomment and update to find current packages
-# apt-get update
-
+apt-get update
+sudo apt-get install -y cuda-libraries-9-0
 # Required for demo script! #
 pip install scikit-video
 
 # Add your desired packages for each workspace initialization
 #          Add here!          #
+conda install opencv==3.3.1
 pip install pandas
 pip install scikit-learn
-pip install opencv
-pip install tensorflow==1.5.0
+pip install pillow
+pip install tensorflow-gpu==1.8.0
 pip install keras
